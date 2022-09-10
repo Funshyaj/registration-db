53b1b407db64dd059d3821a3847c9a2aa2d913cb
Funshyaj <funshoJyi29@gmail.com>
2022-09-10 01:10:51 +0100

    update

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getfirestore } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
 
 // Your web app's Firebase configuration
@@ -19,4 +19,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const db = getfirestore(app)
+export const db = getFirestore(app)
